f4695d2d2822ebb6c4c55255db81edf1def1c82d
subhajit bera <subhait16.93@gmail.com>
2022-07-02 18:32:17 +0530

    static

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = [
     'shikshalayaweb.herokuapp.com'
 ]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -121,10 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
